89bb79a6fa574c08dfdc113cc623ccc310c0eafb
dafasr <dafashanrizki2200@gmail.com>
2023-02-21 09:50:48 +0700

    -

--- app/api/tugas/router.js
+++ app/api/tugas/router.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const taskController = require('./controller');
+const { body } = require('express-validator');
 
-// Middleware untuk validasi ID
 const validateId = (req, res, next) => {
   if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
     return res.status(400).json({ message: 'ID Tidak Ada' });
@@ -12,8 +12,27 @@ const validateId = (req, res, next) => {
 
 router.get('/tugas', taskController.getAllTasks);
 router.get('/tugas/:id', validateId, taskController.getTaskById);
-router.post('/tugas', taskController.createTask);
-router.patch('/tugas/:id', validateId, taskController.updateTask);
+router.post(
+  '/tugas',
+  [
+    body('judul').notEmpty().trim().escape(),
+    body('deskripsi').notEmpty().trim().escape(),
+    body('selesai')
+      .isBoolean()
+      .withMessage('Selesai harus bernilai true atau false'),
+  ],
+  taskController.createTask
+);
+router.patch(
+  '/tugas/:id',
+  validateId,
+  [
+    body('judul').notEmpty().trim().escape(),
+    body('deskripsi').notEmpty().trim().escape(),
+    body('selesai').isBoolean().toBoolean(),
+  ],
+  taskController.updateTask
+);
 router.delete('/tugas/:id', validateId, taskController.deleteTask);
 
 module.exports = router;
